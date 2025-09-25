196399f4d696a2b74dfa9af849b553dd02224bc8
Mateo <s225095@dtu.dk>
2025-09-25 11:17:28 +0200

    Updated Skeleton

--- src/components/loading-skeleton.tsx
+++ src/components/loading-skeleton.tsx
@@ -4,6 +4,7 @@ const WeatherSkeleton = () => {
   return (
     <div className='space-y-6'>
       <div className='grid gap-6'>
+        <Skeleton className='h-[100px] w-full rounded-lg mt-10'/>
         <Skeleton className='h-[300px] w-full rounded-lg'/>
         <Skeleton className='h-[300px] w-full rounded-lg'/>
         <div className='grid gap-6 md:grid-cols-2'>
